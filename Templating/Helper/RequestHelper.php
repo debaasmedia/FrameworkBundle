7a877c694b02253d8bfde2a4a29008618e61f7ee
marijn <marijn.huizendveld@gmail.com>
2011-09-14 13:43:08 +0200

    Added inline documentation.

--- Templating/Helper/RequestHelper.php
+++ Templating/Helper/RequestHelper.php
@@ -4,14 +4,31 @@
 
   use Symfony\Bundle\FrameworkBundle\Templating\Helper\RequestHelper as BaseHelper;
 
+  /**
+   * RequestHelper
+   *
+   * @author  Marijn Huizendveld <marijn.huizendveld@gmail.com>
+   */
   class RequestHelper extends BaseHelper
   {
 
+    /**
+     * Get the URI of the request.
+     *
+     * @return  string
+     */
     public function getUri ()
     {
       return $this->request->getRequestUri();
     }
 
+    /**
+     * Get the URI of the request.
+     *
+     * @param   string  $arg_route  Test route to match.
+     *
+     * @return  Boolean
+     */
     public function matchesRoute ($arg_route)
     {
       return preg_match('#(?:' . $arg_route . ')#', $this->getUri());
